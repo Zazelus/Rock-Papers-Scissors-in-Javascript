71eb7917c2785bebe7b8dd5ed92729b5766e3990
Zazelus <mansour091800@gmail.com>
2022-06-03 15:41:29 -0400

    Finished computerPlay() in game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,23 @@
 function computerPlay() {
+    let cpuChoice = Math.floor(Math.random() * 3);
 
+    switch(cpuChoice) {
+        case 0:
+            return 'Rock'
+            break;
+        case 1:
+            return 'Paper'
+            break;
+        case 2:
+            return 'Scissors'
+            break;
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    
 }
 
 function startGame() {
-    
+
 }
