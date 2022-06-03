1d81ff72e680ba0075870d22bec881d3a9fe8748
Zazelus <mansour091800@gmail.com>
2022-06-03 16:02:51 -0400

    Finished startGame() in game.js

--- scripts/game.js
+++ scripts/game.js
@@ -18,23 +18,34 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase()
     computerSelection = computerSelection.toUpperCase()
 
+    let rock = 'ROCK'
+    let paper = 'PAPER'
+    let scissors = 'SCISSORS'
+
     if (playerSelection === computerSelection) {
         return 'It\'s a tie!'
-    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+    } else if (playerSelection === rock && computerSelection === paper) {
         return 'You Lose! Paper beats Rock.'
-    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    } else if (playerSelection === rock && computerSelection === scissors) {
         return 'You Win! Rock beats Scissors.'
-    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+    } else if (playerSelection === paper && computerSelection === rock) {
         return 'You Win! Paper beats Rock.'
-    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+    } else if (playerSelection === paper && computerSelection === scissors) {
         return 'You Lose! Scissors beats Paper'
-    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    } else if (playerSelection === scissors && computerSelection === rock) {
         return 'You Lose! Rock beats Scissors.'
-    } else {
+    } else if (playerSelection === scissors && computerSelection === paper) {
         return 'You Win! Scissors beats Paper'
     }
 }
 
 function startGame() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt('Choose rock, papers or scissors.')
+        let cpuChoice = computerPlay()
 
+        console.log(playRound(playerChoice, cpuChoice))
+    }
 }
+
+startGame()
