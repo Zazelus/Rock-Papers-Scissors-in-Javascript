e5e6cafa0a5610de5d3ce0fcf23cfe4e8778524d
Zazelus <mansour091800@gmail.com>
2022-06-03 15:51:13 -0400

    Finished playRound() in game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,21 +1,38 @@
 function computerPlay() {
-    let cpuChoice = Math.floor(Math.random() * 3);
+    let cpuChoice = Math.floor(Math.random() * 3)
 
     switch(cpuChoice) {
         case 0:
             return 'Rock'
-            break;
+            break
         case 1:
             return 'Paper'
-            break;
+            break
         case 2:
             return 'Scissors'
-            break;
+            break
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    playerSelection = playerSelection.toUpperCase()
+    computerSelection = computerSelection.toUpperCase()
+
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!'
+    } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return 'You Lose! Paper beats Rock.'
+    } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return 'You Win! Rock beats Scissors.'
+    } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return 'You Win! Paper beats Rock.'
+    } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return 'You Lose! Scissors beats Paper'
+    } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return 'You Lose! Rock beats Scissors.'
+    } else {
+        return 'You Win! Scissors beats Paper'
+    }
 }
 
 function startGame() {
